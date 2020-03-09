a351d1f10d0b3e82730facc74e38b0a6beaf1fab
vyaaki1994 <vyaaki1994@gmail.com>
2020-03-09 22:45:52 +0300

    inital commit

---
+++ Controllers/LeaderBoardController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CheekiBreekiWeb.Models;
+using Newtonsoft.Json;
+
+namespace CheekiBreekiWeb.Controllers
+{
+    public class LeaderBoardController : ApiController
+    {
+        static public List<LeaderBoard> leaderBoard = new List<LeaderBoard>();
+        IEnumerable<LeaderBoard> GetFullLeaderBoard()
+        {
+            return leaderBoard;
+        }
+
+        public IHttpActionResult GetLeaderBoard()
+        {
+            return Json(leaderBoard);
+        }
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] dynamic jsonValue)
+        {
+
+            if (jsonValue != null)
+            {
+                leaderBoard.Add(JsonConvert.DeserializeObject<LeaderBoard>(jsonValue.ToString()));
+            }
+            leaderBoard = leaderBoard.OrderBy(o => o.Time).OrderByDescending(o => o.Score).ToList<LeaderBoard>();
+            if (leaderBoard.Count > 10)
+            {
+                leaderBoard.RemoveAt(leaderBoard.Count);
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Models/LeaderBoard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CheekiBreekiWeb.Models
+{
+
+    public class LeaderBoard
+    {
+        public double Time { get; set; }
+        public int Score { get; set; }
+    }
+}
